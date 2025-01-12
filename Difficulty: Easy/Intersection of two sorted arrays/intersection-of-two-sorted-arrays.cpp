956abe3267cfa2e581fcddbe3e69b9a159e667dd
ARPIT SINGH <adoranto737@gmail.com>
2025-01-12 11:49:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the intersection of two arrays.
+    vector<int> intersection(vector<int> &a, vector<int> &b) {
+        unordered_set<int> sa(a.begin(), a.end());  
+        vector<int> res;                            
+        for (int i = 0; i < b.size(); i++) {
+            if (sa.find(b[i]) != sa.end()) {
+                res.push_back(b[i]); 
+                sa.erase(b[i]);  }
+        }
+        return res;
+}
+};
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> arr1, arr2;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            arr1.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            arr2.push_back(number2);
+        }
+        Solution ob;
+        // Function calling
+        vector<int> v;
+        v = ob.intersection(arr1, arr2);
+
+        if (v.size() > 0) {
+            for (int i = 0; i < v.size(); i++)
+                cout << v[i] << " ";
+        } else {
+            cout << "[]";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+        // string tilde;
+        // getline(cin, tilde);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
